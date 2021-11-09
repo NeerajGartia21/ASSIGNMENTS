13e6cfa2e83d5c7809fefbc83e07274c5fe1f207
Neeraj211002 <80708727+Neeraj211002@users.noreply.github.com>
2021-11-10 02:42:33 +0530

    conversion to 2D array implimented

--- json_flatter/script.js
+++ json_flatter/script.js
@@ -1,10 +1,57 @@
-const flattenJSON = (obj = {}, res = {}, extraKey = '') => {
-    for(key in obj){
-       if(typeof obj[key] !== 'object'){
-          res[extraKey + key] = obj[key];
-       }else{
-          flattenJSON(obj[key], res, `${extraKey}${key}.`);
-       };
-    };
-    return res;
+const flattenJson = (obj = {}, output = {}, extraKey = '') => {
+   for(key in obj){
+      if(typeof obj[key] !== 'object'){
+         output[extraKey + key] = obj[key];
+      }else{
+         flattenJson(obj[key], output, `${extraKey}${key}.`);
+      };
+   };
+   return output;
 };
+
+const get2DArray = (obj = {}, output =[], extraKey = '',depth=1,parent="root") => {
+   for(key in obj){
+      if(typeof obj[key] !== 'object'){
+         output.push([depth,parent,key,typeof(obj[key]),obj[key]]);
+      }else if(obj[key] instanceof Array){
+         output.push([depth,parent,key,typeof(obj[key]),obj[key]]);
+      }else{
+         output.push([depth,parent,key,typeof(obj[key])]);
+         parent=key;
+         depth++
+         get2DArray(obj[key], output,extraKey,depth,parent);
+      };
+   };
+   return output;
+};
+
+document.getElementById('get2DArray').addEventListener('click',function(){
+       const output=get2DArray(JSON.parse(document.getElementById('input').value));
+       document.getElementById('output').innerHTML=`<tr>
+       <th>Depth</th>
+       <th>Parent</th>
+       <th>Entity</th>
+       <th>Type</th>
+       <th>Value</th>
+   </tr>`;
+       output.forEach(element=>{
+          if(element[4]){
+            document.getElementById('output').innerHTML+=`<tr>
+            <th>${element[0]}</th>
+            <th>${element[1]}</th>
+            <th>${element[2]}</th>
+            <th>${element[3]}</th>
+            <th>${element[4]}</th>
+          </tr>`;
+          }else{
+            document.getElementById('output').innerHTML+=`<tr>
+            <th>${element[0]}</th>
+            <th>${element[1]}</th>
+            <th>${element[2]}</th>
+            <th>${element[3]}</th>
+          </tr>`;
+          }
+       })
+})
+
+
